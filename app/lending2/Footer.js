b61501e66cf416dab3002d78bed84f90d48e8961
satyasahoo@meelance.com <Satyabrata Sahoo>
2024-12-03 12:51:06 +0530

    footer issue fixed

--- app/lending2/Footer.js
+++ app/lending2/Footer.js
@@ -15,8 +15,7 @@ const Footer = () => {
   return (
     <footer
       data-layername="footer"
-      className="flex flex-wrap gap-5 justify-between py-6 mt-12 bg-white border-t border-black border-opacity-10 max-md:mt-6 max-md:px-4"
-      style={{ paddingLeft: '7rem', paddingRight: '7rem' }}
+      className="flex flex-col gap-5 justify-between py-6 mt-12 bg-white border-t border-black border-opacity-10 max-md:mt-6 w-full px-5"
     >
       <nav className="flex gap-4 items-start px-2.5 py-1.5 text-base text-black whitespace-nowrap max-md:flex-wrap max-md:justify-center max-md:text-sm max-md:gap-3">
         {footerLinks.map((link, index) => (
@@ -34,19 +33,19 @@ const Footer = () => {
       <div className="flex gap-4 justify-center items-center py-1.5 max-md:flex-col md:max-w-full lg:max-w-full">
         <div
           data-layername="layer1"
-          className="flex overflow-hidden text-center item-center py-1 my-auto w-full"
+          className="flex overflow-hidden text-center item-center justify-center py-1 my-auto w-full"
         >
           <Image
             loading="lazy"
             src={meelance_gradient}
             alt="Meelance logo"
-            className="object-contain aspect-[4.56] w-[100px] ml-[3rem]"
+            className="object-contain aspect-[4.56] w-[100px]"
           />
         </div>
-        <div className="justify-center items-center py-1 my-auto lg:ml-auto ml-[-2.8rem] w-full">
+        <div className="justify-center items-center py-1 my-auto lg:ml-auto w-full">
           <p
             data-layername="2024MeelanceIncAllRightsReserved"
-            className="my-auto text-sm font-medium leading-none text-black min-w-[250px] max-md:text-center w-full"
+            className="my-auto text-sm font-medium leading-none text-black max-md:text-center w-full"
           >
             2024 Meelance, Inc. All rights reserved.
           </p>
